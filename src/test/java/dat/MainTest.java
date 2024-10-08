f5e595e7bf9fe9e95ecddb9e7980e0ff784ed677
Alfredo Fernandez <aalagt@outlook.dk>
2024-10-08 10:51:05 +0200

    first commit

---
+++ src/test/java/dat/MainTest.java
@@ -0,0 +1,25 @@
+package dat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        int a = 1;
+
+        assertEquals(1, a);
+    }
+}
